fedc437cb54bc81e0dc5ec96fffd240a73ff6636
Minh Chau <minhchau@xpierapp.com>
2025-03-24 08:32:09 +0100

    work with BigDecimal

--- src/main/java/com/example/jtoolbox/commons/NumberUtils.java
+++ src/main/java/com/example/jtoolbox/commons/NumberUtils.java
@@ -1,5 +1,7 @@
 package com.example.jtoolbox.commons;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
@@ -7,9 +9,18 @@ import java.util.Locale;
 public class NumberUtils {
 
   public static final DecimalFormat DECIMAL_AMOUNT_FORMAT = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.ENGLISH));
+  public static final DecimalFormat DECIMAL_AMOUNT_FORMAT_GERMAN = new DecimalFormat("#,##0.00");
 
   public static double formatDouble(final double value) {
     return Double.parseDouble(DECIMAL_AMOUNT_FORMAT.format(value));
+  }
+
+  public static String formatDoubleGermanAsString(final BigDecimal value) {
+    return DECIMAL_AMOUNT_FORMAT_GERMAN.format(value);
+  }
+
+  public static BigDecimal asBigDecimal(final String value, final int scale) {
+    return new BigDecimal(value).setScale(scale, RoundingMode.HALF_UP);
   }
 
 }
--- src/test/java/com/example/jtoolbox/commons/NumberUtilsTest.java
+++ src/test/java/com/example/jtoolbox/commons/NumberUtilsTest.java
@@ -2,6 +2,7 @@ package com.example.jtoolbox.commons;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 class NumberUtilsTest {
@@ -12,6 +13,22 @@ class NumberUtilsTest {
     assertEquals(3.36, NumberUtils.formatDouble(3.356));
     assertEquals(3.30, NumberUtils.formatDouble(3.3));
     assertEquals(3.00, NumberUtils.formatDouble(3));
+  }
+
+  @Test
+  void testFormatDoubleGermanAsString() {
+    assertEquals("3,33", NumberUtils.formatDoubleGermanAsString(new BigDecimal("3.33")));
+    assertEquals("3,36", NumberUtils.formatDoubleGermanAsString(new BigDecimal("3.356")));
+    assertEquals("3,30", NumberUtils.formatDoubleGermanAsString(new BigDecimal("3.3")));
+    assertEquals("3,00", NumberUtils.formatDoubleGermanAsString(new BigDecimal("3")));
+  }
+
+  @Test
+  void testAsBigDecimal() {
+    assertEquals(new BigDecimal("3.33"), NumberUtils.asBigDecimal("3.333", 2));
+    assertEquals(new BigDecimal("3.36"), NumberUtils.asBigDecimal("3.356", 2));
+    assertEquals(new BigDecimal("3.30"), NumberUtils.asBigDecimal("3.3", 2));
+    assertEquals(new BigDecimal("3.00"), NumberUtils.asBigDecimal("3", 2));
   }
 
 }
